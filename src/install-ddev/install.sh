be746c84f437afe988da74091ba469ef3d770153
Randy Fay <randy@randyfay.com>
2022-12-22 12:02:03 -0700

    Add xdg-utils in installer

--- src/install-ddev/install.sh
+++ src/install-ddev/install.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 DDEV_URL=https://nightly.link/drud/ddev/actions/artifacts/485751722.zip
 echo "Installing DDEV"
 
+sudo apt update && sudo apt install -y curl wget xdg-utils
 sudo apt remove ddev >/dev/null 2>&1 || true
 
 cd /tmp && curl -s -L -O ${DDEV_URL}
