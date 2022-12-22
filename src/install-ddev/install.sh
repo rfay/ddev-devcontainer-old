184a9e224b1706274cc3a7dea9142a96d15b743d
Randy Fay <randy@randyfay.com>
2022-12-22 10:59:17 -0700

    Allow error on apt remove

--- src/install-ddev/install.sh
+++ src/install-ddev/install.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 DDEV_URL=https://nightly.link/drud/ddev/actions/artifacts/485751722.zip
 echo "Installing DDEV"
 
-sudo apt remove ddev >/dev/null 2>&1
+sudo apt remove ddev >/dev/null 2>&1 || true
 
 cd /tmp && curl -L -O ${DDEV_URL}
 zipball=$(basename ${DDEV_URL})
