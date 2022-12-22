96692e3ee7c8b07b0e587fca3d0d47c0059e74b8
Randy Fay <randy@randyfay.com>
2022-12-22 11:05:52 -0700

    Improve install workflow

--- src/install-ddev/install.sh
+++ src/install-ddev/install.sh
@@ -6,7 +6,8 @@ echo "Installing DDEV"
 
 sudo apt remove ddev >/dev/null 2>&1 || true
 
-cd /tmp && curl -L -O ${DDEV_URL}
+cd /tmp && curl -s -L -O ${DDEV_URL}
 zipball=$(basename ${DDEV_URL})
 unzip ${zipball}
-sudo mv ddev /usr/local/bin
+chmod +x ddev && sudo mv ddev /usr/local/bin
+rm -f ${zipball}
